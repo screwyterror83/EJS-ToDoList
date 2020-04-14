1a7709c147bde1dca4def7a4939150b75d6c011f
screwyterror83 <screwyterror83@gmail.com>
2020-04-14 14:32:49 -0700

    EJS package ready

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ const https = require("https");
 
 const app = express();
 
+app.set("view engine", "ejs")
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "html/index.html");
+  
+
+  let today = new Date();
+  let currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yey, it's the weekend ~! </h1>");
+    res.write("<p>That means I don't have to work.</p>");
+  } else {
+    res.sendFile(__dirname + "/html/index.html");
+  }
+  
 })
-
-
-
 
 
 
