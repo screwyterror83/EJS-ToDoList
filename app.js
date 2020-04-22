ae1f8163b6b91f7f64aa392d10bcaa073182b89a
screwyterror83 <screwyterror83@gmail.com>
2020-04-22 16:01:47 -0700

    Add Procfile for Heroku, and updated app.listen

--- app.js
+++ app.js
@@ -284,9 +284,11 @@ app.post("/delete", (req, res) => {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
+  console.log("Todo List server is now running.");
+});
 
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Todo List server is now running.")
-})
